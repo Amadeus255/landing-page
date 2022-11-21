920475fe4fb1f0a4f9114f47adcf196cb18dffb5
Omar Maher <62303329+Amadeus255@users.noreply.github.com>
2022-11-21 18:04:14 +0200

    Finished?

--- js/app.js
+++ js/app.js
@@ -1,62 +1,44 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+// definnig the variables for building the navbar
+const sections = document.querySelectorAll("section");
+const navMenu = document.querySelector("#navbar__list");
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
+// a fragment for improving the performance of the build process
+const fragment = document.createDocumentFragment();
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+// building the navbar
+function buildNavListItems() {
+    for (const section of sections) {
+        const navListItem = document.createElement("li");
+        navListItem.innerHTML = `<a href='#${section.id}' class='menu__link'>${section.dataset.nav}</a>`;
+
+        fragment.appendChild(navListItem);
+    }
+
+    navMenu.appendChild(fragment);
+}
+window.addEventListener("load", buildNavListItems);
 
 
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+// Add class 'active' to section and the navbar links when near top of viewport
+function setActive() {
+    const links = document.querySelectorAll("a.menu__link");
+    for (const section of sections) {
+        if (
+            section.getBoundingClientRect().top <= 500 &&
+            section.getBoundingClientRect().top >= -50
+        ) {
+            section.classList.add("your-active-class");
+            links.forEach((link) => {
+                if (link.textContent === section.dataset.nav) {
+                    link.classList.add("active");
+                } else {
+                    link.classList.remove("active");
+                }
+            });
+        } else {
+            section.classList.remove("your-active-class");
+        }
+    }
+}
+window.addEventListener("scroll", setActive);
